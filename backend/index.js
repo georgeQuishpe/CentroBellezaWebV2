9097076332d9e75d1f02291a3f7de25eb1bf365a
Joel Guingla <luis.guingla@epn.edu.ec>
2024-11-26 00:57:06 -0500

    feat-backend: integrate Socket.IO for real-time communication and update server setup

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,24 @@
 // backend/index.js
 const express = require('express');
 const cors = require('cors');
+const { createServer } = require('http');
+const { Server } = require('socket.io');
 const routerApi = require('./routes');
 
 const app = express();
-const port = 5000;
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: "http://localhost:5173", // URL de tu frontend en Vite
+    methods: ["GET", "POST"]
+  }
+});
 
 app.use(cors());
 app.use(express.json());
+
+// Configuración de WebSocket
+require('./websocket/chat.socket')(io);
 
 app.get('/', (req, res) => {
   res.send('Conexión exitosa con el backend!');
@@ -15,7 +26,7 @@ app.get('/', (req, res) => {
 
 routerApi(app);
 
-app.listen(port, () => {
+const port = process.env.PORT || 5000;
+httpServer.listen(port, () => {
   console.log(`Backend funcionando en http://localhost:${port}`);
 });
-
