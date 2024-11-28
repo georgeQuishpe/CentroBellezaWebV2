69a9d0218cb630cecf11a29415a85eaec71c9acf
George Quishpe <george.quishpe@epn.edu.ec>
2024-11-27 22:41:43 -0500

    Fix: Backend of Services are fixed

--- backend/routes/services.router.js
+++ backend/routes/services.router.js
@@ -3,16 +3,16 @@ const router = express.Router();
 const servicesController = require('../controllers/service.controller');
 
 
-const isAdmin = (req, res, next) => {
-    if (req.user?.rol !== 'Admin') {
-        return res.status(403).json({ success: false, message: 'Acceso denegado' });
-    }
-    next();
-};
+// const isAdmin = (req, res, next) => {
+//     if (req.user?.rol !== 'Admin') {
+//         return res.status(403).json({ success: false, message: 'Acceso denegado' });
+//     }
+//     next();
+// };
 
-router.post('/', isAdmin, servicesController.create);
-router.put('/:id', isAdmin, servicesController.update);
-router.delete('/:id', isAdmin, servicesController.remove);
+// router.post('/', isAdmin, servicesController.create);
+// router.put('/:id', isAdmin, servicesController.update);
+// router.delete('/:id', isAdmin, servicesController.remove);
 
 
 router
